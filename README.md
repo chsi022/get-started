0c553a7c2d331cd1b5ea83b409584b6bbb9846df
achen <allen@brightlabs.com.au>
2016-09-25 22:38:38 +1000

    Finished technical test, commit WIP project

--- README.md
+++ README.md
@@ -1,2 +1,48 @@
-# get-started
-Allen's Front End Developer Technical Test
+#=================================
+# READ ME
+#=================================
+
+This is the Get Started's Front End Developer Technical Test done by Allen Chen. 
+The repo can be found on https://github.com/chsi022/get-started.git
+
+
+#=================================
+# Progress report
+#=================================
+
+The following elements are finished with responsive support
+- Header / navigation
+- Search
+- Hero carousel
+- Intro text area
+- Services listing
+
+The following elements still need to be done
+- Blog carousel
+- Footer
+
+The following areas should be refined 
+- Add in task running (grunt) to combine and minify JS / CSS files
+- Proper cross browser testing still needs to be carried out
+- Strip out un-needed bootstrap scripts / favion / fonts that came with the HTML5 Initialzr boilerplate
+- The cutup is not done pixel perfect, need to check and refine implementation
+
+
+#=================================
+# Tools / Plugins used
+#=================================
+
+1. 
+SASS is used for CSS precompilation. SASS must be installed if changes to the styles are to be carried out. Please find the installation guide here http://sass-lang.com/install
+
+2.
+The JS plugin MMenu (http://mmenu.frebsite.nl/) is used for mobile navigation. This is one of the mobile menu plugin that I am familiar with and it offers a wide range of options for customisation.
+
+3. 
+The JS plugin carouFredSel (https://github.com/gilbitron/carouFredSel) is used for the hero carousel, and is fully capable to be used to implement the blog carousel. Again, this is the carousel plugin that I am most familiar with, hance it was pick for this project.
+
+4.
+This project was created with a HTML5 boilerplate from Initializr (https://html5boilerplate.com/). It was picked to quickly get the project up and running. It was customised with Modernizr, jquery and Twitter bootstrap for responsive support.
+
+5.
+The SASS mixin library bourbon (http://bourbon.io/) and css-reset-and-normalize-sass (https://www.npmjs.com/package/css-reset-and-normalize-sass) was added to the project to kick off the styling work. There is no special reasoning to why these 2 were picked over the others - I simply needed some basic mixins and style resets and these came up first in my goole search.
--- js/main.js
+++ js/main.js
@@ -1,1 +1,171 @@
+var main = {
+	
+	init: function()
+	{
+		main.initMobileNav();
+		main.initSearch();
+		main.initInputValueDetection();
+		main.initCarousels();
+	},
 
+	/**
+	 * Initialise Generic Carousels
+	 */
+	initCarousels: function()
+	{
+		main.initCarouselOnImageLoad($('.hero-carousel__list'), main.initHomeHeroCarousel);
+	},
+
+	/**
+	 * Function to init carousel after the first image in it has loaded.
+	 * This function will help to reduce the incomplete carousel load issue.
+	 */
+	initCarouselOnImageLoad: function(carousel, callback)
+	{
+		if (!(carousel instanceof jQuery)){
+			carousel = jQuery(carousel);
+		}
+		if (!carousel.length || !callback){ return false; }
+
+		var carousel_img = carousel.find('img').first();
+		if (!carousel_img.length){ return false; }
+
+		if (carousel_img.prop('complete')){
+			callback();
+		}
+		else {
+			carousel_img.load(callback);
+		}
+	},
+
+
+	/**
+	 * Init the Home hero carousel
+	 */
+	initHomeHeroCarousel: function()
+	{
+		if (!$('.hero-carousel__list').length){ return false; }
+
+		$('.hero-carousel__list').carouFredSel({
+			auto: 5000,
+			responsive: true,
+			items: {
+				visible: 1,
+				height: 'variable'
+			},
+			swipe: {
+					onTouch     : true,
+					onMouse     : false
+			},
+			align: 'center',
+			circular: true,
+			pagination: '.hero-carousel__pager'
+		});
+	},
+
+
+	/**
+	 * Init the Mobile menu, with its search function and mobile menu button
+	 */
+	initMobileNav: function()
+	{
+		if (!$('#menu').length){ return false; }
+
+		// Initialize MMenu Plugin
+		var menu = $('#menu').mmenu(null, {
+				clone: true
+	        });
+
+		// Open MMenu when mobile menu icon is clicked
+		$('.navigation__trigger-mobile-nav').first().on('click touchstart', function(event){
+			event.preventDefault();
+			$('#mm-menu').mmenu({
+				position: 'right',
+				searchfield: {
+					add: true,
+	            	search: false
+	            }
+        	}).trigger('open.mm');
+		});
+
+		// Override the mmenu search function
+		$("#mm-menu .mm-search input")
+			.bind("change", function() {				
+				if ($(this).val()){
+					// do the search
+				    $('.search__container form .search__input').val($(this).val());
+				   	$('.search__container form').submit();
+				    
+				    // close the menu?
+				    menu.mmenu().trigger( "close" );
+				}			    
+			}
+		);
+	},
+
+	/**
+	 * Adds a has-input class to the input / textarea when the element
+	 * contains user input, this will help with styling material design 
+	 * label
+	 */
+	initInputValueDetection: function()
+	{
+		if (!$('input, textarea').length){ return false; }
+
+		$('input, textarea').each(function(){
+			$(this).change(function(){
+				if ($(this).val() !== ''){
+					$(this).addClass('has-input');
+				} else {
+					$(this).removeClass('has-input');
+				}
+			});
+			$(this).trigger('change');
+		});
+	},
+
+	/**
+	 * Init the search box, to make it show / hide based on if the
+	 * search input is focused upon or not.
+	 */
+	initSearch: function()
+	{
+		if (!$('.search__input').length){ return false; }
+
+		// Show search when input is focused
+		$('.search__input').focus(function(){
+			$('.search').addClass('active');
+		});
+
+		// Hide search when input loses focus - delay it by half a second
+		// to allow the search submit to be pressed
+		$('.search__input').blur(function(){
+			setTimeout(function(){
+				$('.search').removeClass('active');
+			}, 300);
+		});
+
+		// Focus the search input when the search trigger is clicked upon
+		$('.search__trigger').click(function(){
+			$('.search__input').focus();
+		});
+
+		// Check empty submit to close the search
+		$('.search__submit').click(function(e){
+			if ($('.search').hasClass('active')){
+				if (!$('.search__input').val()) {
+					e.preventDefault();
+					$('.search').removeClass('active');
+				}
+			} else {
+				e.preventDefault();
+				$('.search__input').focus();
+			}
+		});
+	}
+};
+
+// Run the main js
+$(function(){
+	main.init();
+});
